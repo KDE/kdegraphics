ed704aa3ac33ec3dbf4b9b0ba76d35af5461582b
Laurent Montel <montel@kde.org>
2007-04-11 15:46:31 +0000

    Fix include
    svn path=/trunk/KDE/kdegraphics/; revision=652627

--- config.h.cmake
+++ config.h.cmake
@@ -186,9 +186,6 @@
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
-/* Define the location your xpdfrc */
-#define SYSTEM_XPDFRC "/etc/xpdfrc"
-
 /* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
 #define TIME_WITH_SYS_TIME 1
 
